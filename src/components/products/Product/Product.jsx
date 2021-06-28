fc4d20b309c13862b78d41ca63d862553d5faa29
Akash <akashleo2009@gmail.com>
2021-06-28 23:02:59 +0530

    mock products

---
+++ src/components/products/Product/Product.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Card, CardMedia, CardContent, CardActions, Typography, IconButton} from '@material-ui/core'; 
+import { AddShoppingCart } from '@material-ui/icons';
+
+const Product = () => {
+    return (
+        <Card className={classes.root}>
+            <CardMedia/>
+            
+        </Card>
+    )
+}
+
+export default Product
