2700fbe1f107cd3a9d07465734dc290da6125ddb
Akash <akashleo2009@gmail.com>
2021-06-29 00:36:13 +0530

    mock products

--- src/components/products/Product/Product.jsx
+++ src/components/products/Product/Product.jsx
@@ -1,14 +1,28 @@
-import React from 'react'
-import { Card, CardMedia, CardContent, CardActions, Typography, IconButton} from '@material-ui/core'; 
-import { AddShoppingCart } from '@material-ui/icons';
+import React from "react";
+import {
+  Card,
+  CardMedia,
+  CardContent,
+  CardActions,
+  Typography,
+  IconButton,
+} from "@material-ui/core";
+import { AddShoppingCart } from "@material-ui/icons";
 
 const Product = () => {
-    return (
-        <Card className={classes.root}>
-            <CardMedia/>
-            
-        </Card>
-    )
-}
+  return (
+    <Card className={classes.root}>
+      <CardMedia className={classes.media} image="" title={product.name} />
+      <CardContent>
+        <div className={classes.CardContent}>
+          <Typography variant="h5" gutterBottom>
+            {product.name}
+          </Typography>
+          <Typography variant="h5">{product.price}</Typography>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
 
-export default Product
+export default Product;
