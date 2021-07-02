5765fb4a9e6862a713a1c43340f54bef77c5ab23
Akash <akashleo2009@gmail.com>
2021-07-03 00:52:13 +0530

    navbar complete

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,34 +1,47 @@
 import React from "react";
 import {
-    AppBar,
-    Toolbar,
-    IconButton,
-    Badge,
-    MenuItem,
-    Menu,
-    Typography,
+  AppBar,
+  Toolbar,
+  IconButton,
+  Badge,
+  MenuItem,
+  Menu,
+  Typography,
 } from "@material-ui/core";
 import { ShoppingCart } from "@material-ui/icons";
 
-import logo from '../../assets/commerce.png';
+import logo from "../../assets/commerce.png";
+
+import useStyles from "./styles";
 
 const Navbar = () => {
-    return (
-        <div>
-            <AppBar position="fixed" className={classes.appBar} color="inherit">
-                <Toolbar >
-                    <Typography variant="h6" className={classes.title} color="inherit">
-                        <img src={ logo} alt="Commerce.js" height="25px" className={classes.image} />
-                        Commerce App
-                    </Typography>
-                    <div className={classes.grow}/>
-                    <div className={classes.button}>
-                    
-                    </div>
-                </Toolbar>
-            </AppBar>
-        </div>
-    );
+  const classes = useStyles();
+
+  return (
+    <div>
+      <AppBar position="fixed" className={classes.appBar} color="inherit">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title} color="inherit">
+            <img
+              src={logo}
+              alt="Commerce.js"
+              height="25px"
+              className={classes.image}
+            />
+            Commerce App
+          </Typography>
+          <div className={classes.grow} />
+          <div className={classes.button}>
+            <IconButton aria-label="Show cart items" color="inherit">
+              <Badge badgeContent={2} color="secondary">
+                <ShoppingCart />
+              </Badge>
+            </IconButton>
+          </div>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 };
 
 export default Navbar;
--- src/components/products/products.jsx
+++ src/components/products/products.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Grid } from '@material-ui/core';
 
 import Product from './Product/Product';
+import useStyles from './styles';
 
 const products = [
     { id: 1, name: 'Shoes', description: 'Running Shoes.', price: "$5" , image: "https://www.saucony.com/on/demandware.static/-/Sites-saucony_us-Library/default/dw6ca64fdc/content/seasonal-content/homepage/2021/05/endorphinshift-d.jpg"},
@@ -9,7 +10,10 @@ const products = [
 ];
 
 const Products = () => {
-    return (<main>
+    const classes = useStyles();
+
+    return (<main classeName={classes.content}>
+    <div className={classes.toolbar}/>
         <Grid container justify="center" spacing={4}>
             {products.map((product) => (
                 <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
