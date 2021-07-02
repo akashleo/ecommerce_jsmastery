196bf9fd4eb503956ebb917ce6ee40ffdce02c5e
Akash <akashleo2009@gmail.com>
2021-07-02 23:13:45 +0530

    added product images

--- src/components/products/products.jsx
+++ src/components/products/products.jsx
@@ -4,8 +4,8 @@ import { Grid } from '@material-ui/core';
 import Product from './Product/Product';
 
 const products = [
-    { id: 1, name: 'Shoes', description: 'Running Shoes.', price: "$5" },
-    { id: 1, name: 'Macbook', description: 'Apple Macbook.', price: "$15" }
+    { id: 1, name: 'Shoes', description: 'Running Shoes.', price: "$5" , image: "https://www.saucony.com/on/demandware.static/-/Sites-saucony_us-Library/default/dw6ca64fdc/content/seasonal-content/homepage/2021/05/endorphinshift-d.jpg"},
+    { id: 1, name: 'Macbook', description: 'Apple Macbook.', price: "$15", image: "https://cdn.mos.cms.futurecdn.net/uWjEogFLUTBc8mSvagdiuP-1024-80.jpg.webp" }
 ];
 
 const Products = () => {
